96f080f84246c938c8f4d9c6c7b6c48f2834f574
Leah <leahwhughes@gmail.com>
2020-10-11 15:57:38 -0700

    tiny comment update

--- lib/possible_bipartition.rb
+++ lib/possible_bipartition.rb
@@ -20,11 +20,11 @@ def possible_bipartition(dislikes)
       curr_dog = stack.pop # pop one out
       disliked_dogs = dislikes[curr_dog] # identify who the current dog doesn't like 
   
-      disliked_dogs.each do |dog| # look at the current dog's dislikes [2,3]
+      disliked_dogs.each do |dog| # look at the current dog's dislikes
         if !visited[dog] # if we haven't seen that dog before
           stack << dog # put it on the stack
           visited[dog] = which_pen(visited[curr_dog]) # assign the dog a pen
-        elsif visited[dog] == visited[curr_dog] # if the current dog is assigned to the same pen as a dog it dislikes
+        elsif visited[dog] == visited[curr_dog] # (if we HAVE seen the dog before) && if the current dog is assigned to the same pen as a dog it dislikes
           return false # we can't do it!
         end
       end
