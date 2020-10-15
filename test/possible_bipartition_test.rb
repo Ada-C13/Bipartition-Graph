df56cae7f5ad3ceed32691c8e888089999ffa897
Hannah Tekié <hannahtekie@gmail.com>
2020-10-14 20:47:52 -0700

    all test passed

--- test/possible_bipartition_test.rb
+++ test/possible_bipartition_test.rb
@@ -3,12 +3,11 @@ require_relative "test_helper"
 describe "possible_bipartition" do
   it "will work for example 1" do
     # Arrange
-    dislikes = [ [],
-      [2, 3],
-      [1, 4],
-      [1],
-      [2]
-    ]
+    dislikes = [[],
+                [2, 3],
+                [1, 4],
+                [1],
+                [2]]
 
     # Act
     answer = possible_bipartition(dislikes)
@@ -19,11 +18,10 @@ describe "possible_bipartition" do
 
   it "will work for example 2" do
     # Arrange
-    dislikes =  [ [],
-      [2, 3],
-      [1, 3],
-      [1, 2]
-    ]
+    dislikes = [[],
+                [2, 3],
+                [1, 3],
+                [1, 2]]
 
     # Act
     answer = possible_bipartition(dislikes)
@@ -34,13 +32,12 @@ describe "possible_bipartition" do
 
   it "will work for example 3" do
     # Arrange
-    dislikes = [ [],
-      [2, 5],
-      [1, 3],
-      [2, 4],
-      [3, 5],
-      [1, 4]
-    ]
+    dislikes = [[],
+                [2, 5],
+                [1, 3],
+                [2, 4],
+                [3, 5],
+                [1, 4]]
 
     # Act
     answer = possible_bipartition(dislikes)
@@ -51,14 +48,13 @@ describe "possible_bipartition" do
 
   it "will return true for a graph which can be bipartitioned" do
     # Arrange
-    dislikes = [ [3, 6],
-      [2, 5],
-      [1, 3],
-      [0, 2],
-      [5],
-      [1, 4],
-      [0]
-    ]
+    dislikes = [[3, 6],
+                [2, 5],
+                [1, 3],
+                [0, 2],
+                [5],
+                [1, 4],
+                [0]]
 
     # Act
     answer = possible_bipartition(dislikes)
@@ -67,17 +63,15 @@ describe "possible_bipartition" do
     expect(answer).must_equal true
   end
 
-
   it "will return false for a graph which cannot be bipartitioned" do
     # Arrange
-    dislikes = [ [3, 6],
-      [2, 5],
-      [1, 3],
-      [0, 2, 4],
-      [3, 5],
-      [1, 4],
-      [0]
-    ]
+    dislikes = [[3, 6],
+                [2, 5],
+                [1, 3],
+                [0, 2, 4],
+                [3, 5],
+                [1, 4],
+                [0]]
 
     # Act
     answer = possible_bipartition(dislikes)
@@ -89,19 +83,18 @@ describe "possible_bipartition" do
   it "will work for an empty graph" do
     expect(possible_bipartition([])).must_equal true
   end
-  
-    it "will return false for a graph which cannot be bipartitioned" do
+
+  it "will return false for a graph which cannot be bipartitioned" do
     # Arrange
-    dislikes = [ [3, 6],
-      [2, 5],
-      [1, 3],
-      [0, 2, 4],
-      [3, 5],
-      [1, 4],
-      [0],
-      [8],
-      [7]
-    ]
+    dislikes = [[3, 6],
+                [2, 5],
+                [1, 3],
+                [0, 2, 4],
+                [3, 5],
+                [1, 4],
+                [0],
+                [8],
+                [7]]
 
     # Act
     answer = possible_bipartition(dislikes)
