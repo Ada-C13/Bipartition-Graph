189c7c67a918b5c3ffd0b2a813362f57d665fd49
vwhwang <vwhwang38@gmail.com>
2020-11-28 13:28:11 -0800

    completed

--- lib/possible_bipartition.rb
+++ lib/possible_bipartition.rb
@@ -1,4 +1,40 @@
 
+#   dislikes = [ [],
+#   [2, 3],
+#   [1, 4],
+#   [1],
+#   [2]
+# ]
+
 def possible_bipartition(dislikes)
-  raise NotImplementedError, "possible_bipartition isn't implemented yet"
-end
+  dislikes.each_index do |dog|
+    if helper(dislikes,dog) == false 
+      return false 
+    end 
+  end 
+  return true 
+end 
+
+
+def helper(dislikes, node)
+  visited = {}
+
+  queue = []
+  queue.push(node)
+ 
+  visited[node] = "red"
+  while !queue.empty? 
+    current = queue[0]
+    queue = queue.drop(1)
+  
+    dislikes[current].each do |neighbor|
+      if visited[neighbor]
+        return false if visited[current] == visited[neighbor]
+      else 
+        visited[neighbor] = visited[current] == "red" ? "blue": "red"
+        queue.push(neighbor)
+      end 
+    end 
+  end
+  return true 
+end 
